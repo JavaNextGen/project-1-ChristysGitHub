aebb537cabf356a11b9d2ae333284432785ecd34
ChristysGitHub <chrndaye@gmail.com>
2022-01-13 00:34:20 -0800

    database structure almost complete

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -16,10 +16,12 @@ package com.revature.models;
  */
 public class User extends AbstractUser {
 	
+	private int user_id;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String phoneNumber;
+	private int role_id;
 
     public User() {
         super();
@@ -33,12 +35,22 @@ public class User extends AbstractUser {
         super(id, username, password, role);
     }
     
-    public User(String firstName, String lastName, String email, String phoneNumber) {
+    public User(int user_id, String firstName, String lastName, String email, String phoneNumber, int role_id) {
+    	this.user_id = user_id;
     	this.firstName = firstName;
     	this.lastName = lastName;
     	this.email = email;
     	this.phoneNumber = phoneNumber;
+    	this.role_id = role_id;
     }
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -71,6 +83,71 @@ public class User extends AbstractUser {
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
+	public int getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", phoneNumber=" + phoneNumber + ", role_id=" + role_id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+		result = prime * result + role_id;
+		result = prime * result + user_id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (phoneNumber == null) {
+			if (other.phoneNumber != null)
+				return false;
+		} else if (!phoneNumber.equals(other.phoneNumber))
+			return false;
+		if (role_id != other.role_id)
+			return false;
+		if (user_id != other.user_id)
+			return false;
+		return true;
+	}
+
+	
     
     
 }
